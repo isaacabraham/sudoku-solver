1feef07bee95133a797bd9149a5f7d8f8c06e90f
Isaac Abraham <isaac.abraham@gmail.com>
2014-08-17 18:05:42 +0100

    Some simplification of flattening.

--- SodokuSolver.Logic/Api/SudokuController.fs
+++ SodokuSolver.Logic/Api/SudokuController.fs
@@ -9,22 +9,21 @@ open System.Web.Http
 type SudokuController() = 
     inherit ApiController()
     member __.Post(request : SolutionRequest) = 
-        request.Data
-        |> ParStream.ofSeq
-        |> ParStream.collect (fun verticalBands -> 
-               verticalBands
-               |> Seq.collect (fun horizontalBands -> horizontalBands |> Seq.collect id)
-               |> Stream.ofSeq)
-        |> ParStream.map toCell
-        |> ParStream.toArray
-        |> Solve
-        |> fun (grid, succeeded) -> 
-            { Grid = 
-                  grid
-                  |> Option.map (ParStream.ofSeq
-                                 >> ParStream.map toRequest
-                                 >> ParStream.toArray)
-                  |> function 
-                  | Some solution -> solution
-                  | None -> Array.empty
-              Result = succeeded }
+        async {
+            return request.Data
+                   |> ParStream.ofSeq
+                   |> ParStream.collect (Seq.collect (Seq.collect id) >> Stream.ofSeq)
+                   |> ParStream.map toCell
+                   |> ParStream.toArray
+                   |> Solve
+                   |> fun (grid, succeeded) -> 
+                       { Grid = 
+                             grid
+                             |> Option.map (ParStream.ofSeq
+                                            >> ParStream.map toRequest
+                                            >> ParStream.toArray)
+                             |> function 
+                             | Some solution -> solution
+                             | None -> Array.empty
+                         Result = succeeded }
+        } |> Async.StartAsTask
