837bc232a0f51f9e2e39ef5990201412ae594a19
isaacabraham <isaac.abraham@gmail.com>
2014-09-04 12:47:39 +0100

    Ignore values not between 1 and 9.

--- SodokuSolver.Logic/Api/DomainModel.fs
+++ SodokuSolver.Logic/Api/DomainModel.fs
@@ -8,8 +8,8 @@ let toCell (request : CellRequest) =
       Y = request.Y
       Value = 
           match request.Value |> Int32.TryParse with
-          | true, value -> Some value
-          | false, _ -> None }
+          | true, value when value < 10 && value > 0 -> Some value
+          | _ -> None }
     
 let toRequest (cell : Cell) = 
     { CellRequest.X = cell.X
